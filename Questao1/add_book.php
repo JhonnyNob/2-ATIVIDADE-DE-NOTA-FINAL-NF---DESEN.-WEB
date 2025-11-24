8ca4a0063c156701c1784f8b7535b0b069cf4aaa
Jhonny_nob <jhonatammelloteixeirasouza2@gmail.com>
2025-11-24 18:22:58 -0300

    Implement book addition functionality

--- Questao1/add_book.php
+++ Questao1/add_book.php
@@ -1,1 +1,19 @@
+<?php
+require_once "database.php";
 
+if (isset($_POST["add_book"])) {
+
+    $ID = $_POST["ID"];
+    $titulo = $_POST["titulo"];
+    $autor = $_POST["autor"];
+    $ano = $_POST["ano"];
+
+    $stmt = $conexao->prepare("INSERT INTO livros (ID, titulo, autor, ano) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("issi", $ID, $titulo, $autor, $ano);
+
+    $stmt->execute();
+}
+
+header("Location: index.php");
+exit;
+?>
