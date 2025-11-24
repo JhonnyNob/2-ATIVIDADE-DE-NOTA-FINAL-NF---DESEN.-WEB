857959f8109789e68d5f460b51efe2a3817737a9
Jhonny_nob <jhonatammelloteixeirasouza2@gmail.com>
2025-11-24 18:22:15 -0300

    Implement book deletion functionality

--- Questao1/delete_book.php
+++ Questao1/delete_book.php
@@ -1,1 +1,16 @@
+<?php
+require_once "database.php";
 
+if (isset($_POST["delete_book"])) {
+
+    $ID = $_POST["ID"];
+
+    $stmt = $conexao->prepare("DELETE FROM livros WHERE ID = ?");
+    $stmt->bind_param("i", $ID);
+
+    $stmt->execute();
+}
+
+header("Location: index.php");
+exit;
+?>
