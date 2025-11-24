853aade4b58752e9fc53c01c9a9b93839b04d2b9
Jhonny_nob <jhonatammelloteixeirasouza2@gmail.com>
2025-11-24 19:56:45 -0300

    Update add_tarefa.php

--- Questao2/add_tarefa.php
+++ Questao2/add_tarefa.php
@@ -1,1 +1,32 @@
+<?php
 
+include 'database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $descricao = isset($_POST['descricao']) ? trim($_POST['descricao']) : '';
+    $vencimento = isset($_POST['vencimento']) ? trim($_POST['vencimento']) : '';
+
+    if ($descricao === '' || $vencimento === '') {
+        header("Location: index.php?msg=" . urlencode("Preencha todos os campos!"));
+        exit;
+    }
+
+    $sql = "INSERT INTO tarefas (descricao, vencimento) VALUES (?, ?)";
+    $stmt = $conexao->prepare($sql);
+    if (!$stmt) {
+        die("Erro no prepare: " . $conexao->error);
+    }
+    $stmt->bind_param("ss", $descricao, $vencimento);
+
+    if ($stmt->execute()) {
+        header("Location: index.php?msg=" . urlencode("Tarefa adicionada com sucesso!"));
+        exit;
+    } else {
+        header("Location: index.php?msg=" . urlencode("Erro ao inserir: " . $conexao->error));
+        exit;
+    }
+} else {
+    header("Location: index.php");
+    exit;
+}
+?>
